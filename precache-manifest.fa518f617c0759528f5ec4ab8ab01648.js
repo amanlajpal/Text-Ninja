b64e46f68f905d1b8f3a96bf416a62ab0dd08f00
Aman Lajpal <42869088+amanlajpal@users.noreply.github.com>
2022-01-26 16:28:04 +0530

    Updates

---
+++ precache-manifest.fa518f617c0759528f5ec4ab8ab01648.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b10cbdacb1ca8fd66594d66347d6269a",
+    "url": "/Text-Ninja/index.html"
+  },
+  {
+    "revision": "33df0f4a15c4658bfd45",
+    "url": "/Text-Ninja/static/css/main.076405c9.chunk.css"
+  },
+  {
+    "revision": "8b8d5a007a02500c6c63",
+    "url": "/Text-Ninja/static/js/2.624f555d.chunk.js"
+  },
+  {
+    "revision": "f589bf591c94edd204e3",
+    "url": "/Text-Ninja/static/js/3.d439ba6f.chunk.js"
+  },
+  {
+    "revision": "33df0f4a15c4658bfd45",
+    "url": "/Text-Ninja/static/js/main.125187b6.chunk.js"
+  },
+  {
+    "revision": "25f5864ccfd924993046",
+    "url": "/Text-Ninja/static/js/runtime~main.7738b1a2.js"
+  }
+]);
